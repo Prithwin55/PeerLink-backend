de1574ec35ddfb93b3ddf0d7678503f4d73c41fa
Prithwin <tst@gmail.com>
2024-11-18 13:07:13 +0530

    created comment controller

--- src/main/java/com/peerlink_backend/peerlink_backend/data/service/comment/CommentService.java
+++ src/main/java/com/peerlink_backend/peerlink_backend/data/service/comment/CommentService.java
@@ -1,4 +1,8 @@
 package com.peerlink_backend.peerlink_backend.data.service.comment;
 
+import com.peerlink_backend.peerlink_backend.api.dto.comment.CommentDto;
+
 public interface CommentService {
+
+    CommentDto createComment(CommentDto commentDto,long postID,String jwt);
 }
--- src/main/java/com/peerlink_backend/peerlink_backend/data/service/comment/impl/CommentServiceImpl.java
+++ src/main/java/com/peerlink_backend/peerlink_backend/data/service/comment/impl/CommentServiceImpl.java
@@ -1,12 +1,37 @@
 package com.peerlink_backend.peerlink_backend.data.service.comment.impl;
 
+import com.peerlink_backend.peerlink_backend.api.dto.comment.CommentDto;
+import com.peerlink_backend.peerlink_backend.api.jwt.JwtProvider;
+import com.peerlink_backend.peerlink_backend.api.mapper.comment.CommentMapper;
+import com.peerlink_backend.peerlink_backend.data.entity.comment.Comment;
+import com.peerlink_backend.peerlink_backend.data.entity.post.Post;
+import com.peerlink_backend.peerlink_backend.data.entity.user.User;
 import com.peerlink_backend.peerlink_backend.data.repository.comment.CommentRepository;
+import com.peerlink_backend.peerlink_backend.data.repository.post.PostRepository;
+import com.peerlink_backend.peerlink_backend.data.repository.user.UserRepository;
 import com.peerlink_backend.peerlink_backend.data.service.comment.CommentService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class CommentServiceImpl implements CommentService {
     CommentRepository commentRepository;
+    UserRepository userRepository;
+    PostRepository postRepository;
+
+    @Override
+    public CommentDto createComment(CommentDto commentDto, long postID, String jwt) {
+        String email= JwtProvider.getEmailFromJwtToken(jwt);
+        User user=userRepository.findByEmail(email)
+                .orElseThrow(()->new ResourceNotFoundException("User Not Found"));
+        Post post=postRepository.findById(postID)
+                .orElseThrow(()->new ResourceNotFoundException("Post Not Found"));
+        Comment comment=new Comment(commentDto.getId(), commentDto.getContent(), user,post);
+        commentRepository.save(comment);
+        return CommentMapper.INSTANCE.mapToDto(comment);
+    }
 }
